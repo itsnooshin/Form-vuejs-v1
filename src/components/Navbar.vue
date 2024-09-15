96ec86a31e67a64e6024a4450965aa97666ecb20
itsnooshin <nooshindev@gmail.com>
2024-09-15 22:48:39 +0330

    feat(ui):add form for register address

---
+++ src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <a href="/">
+      <img alt="achareh logo" src="../assets/images/achare-logo.svg" />
+    </a>
+    <nav class="header_navigation">
+      <ul>
+        <li>
+          <RouterLink
+            to="/"
+            active-class="is-active"
+            class="header_navigation-register"
+            >ثبت آدرس</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            to="/view"
+            active-class="is-active"
+            class="header_navigation-view"
+            > مشاهده آدرس ها</RouterLink
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 17px;
+}
+.header_navigation ul {
+  display: flex;
+
+  gap: 21px;
+  font-weight: 700;
+  font-size: 12px;
+}
+
+a {
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+}
+
+main {
+  background-color: #f5f5f5;
+  height: 100vh;
+}
+.header_navigation-register {
+  color: #00bfa5;
+  line-height: 19.53px;
+  letter-spacing: 1px;
+}
+.header_navigation-view {
+  color: #00bfa5;
+  line-height: 19.53px;
+  letter-spacing: 1px;
+}
+
+.is-active {
+  color: #37474f;
+}
+</style>
